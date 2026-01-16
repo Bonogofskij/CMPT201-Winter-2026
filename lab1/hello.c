030aefdc2bda66975dec2e5f8d23061caa5a6c7a
Bonogofskij <bonogofskij@mymacewan.ca>
2026-01-16 18:09:20 +0000

    added ID box to hello.c and cleaned up lab1.txt

--- lab1/hello.c
+++ lab1/hello.c
@@ -1,5 +1,11 @@
-#include <stdio.h>
+/*CMPT201: Lab 1
+June Bonogofski - January 16th, 2026
+hello.c: a simple program that takes no input and prints "Hello World"*/
 
-int main() {
-    printf("Hello World\n");
+#include <stdio.h>  //need to include for printf()
+
+int main() {        //main function
+    printf("Hello World\n");    //does the printing
+
+    return 0;       //returns an int as mian expects
 }
