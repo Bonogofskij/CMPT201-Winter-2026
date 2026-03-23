208e78a8793c5062212b2ad19e863b94fa8502ca
Winter <aurorialis00@gmail.com>
2026-03-23 11:40:13 -0600

    New test for testHTable, updated source code and .txt files for diffing

--- A2/testing/testHTable.c
+++ A2/testing/testHTable.c
@@ -17,10 +17,27 @@ int main() {        //Main function, expects int 0 return
     hashtable create = ht_create();                 //Create a hashtable called 'create'
     char *cPtr = "123ABC";
     ht_insert(create, "New York", cPtr);            //Insert "New York, 123ABC" into create
+    
     int index = hash("New York", create->capacity);
     printf("New York hashes to: %d\n", index);
+    
     void *vPtr = ht_lookup(create, "New York");
     printf("ht_lookup returned pCode: %s\n", *((char**)vPtr));
+    
     ht_print_dist(create);
+    
+    printf("\nNow let's try adding a new node\n");
+    cPtr = "567DEF";
+    ht_insert(create, "California", cPtr);
+
+    index = hash("California", create->capacity);
+    printf("California hashes to: %d\n", index);
+    
+    vPtr = ht_lookup(create, "California");
+    printf("ht_lookup returned pCode: %s\n", *((char**)vPtr));
+
+    printf("Now there should be 2 nodes\n");
+    ht_print_dist(create);
+
     return 0;       //Exits main func
 }
