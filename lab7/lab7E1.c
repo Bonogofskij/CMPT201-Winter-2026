f42e505d77367168196f63b136e308224e115619
Winter <aurorialis00@gmail.com>
2026-03-26 15:12:48 -0600

    Add tar rule to makefile, gameNode func to lab7E1.c

--- lab7/lab7E1.c
+++ lab7/lab7E1.c
@@ -7,13 +7,27 @@ Lecture Instructor’s Name:  Ardy RD
 --------------------------------------------*/
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+//Struct to hold information about each individual game
 typedef struct {
-    double price;
-    char title[25];
-    unsigned int popularity;
-} game;
+    double price;               //Cost
+    char title[25];             //Game title
+    unsigned int popularity;    //int ranking how popular, scale 1-4
+} game;     //Yay typedefs
 
+//Make a 'game' struct with given values, and return a pointer to the struct
+game gameNode(double price, char * title, unsigned int popularity) {
+    game * node = malloc(sizeof(game));     //Allocate memory for the node
+    node->price = price;                    //Set node->price to price
+    strcpy(node->title, title);             //copy title to node->title
+    node->popularity = popularity;          //Set node->popularity to popularity
+
+    return *node;       //Return pointer to the created node
+};
+
+//Compares game popularity based 
 int compGamesByTitle(const void * a, const void * b) {
 
 };
@@ -23,7 +37,5 @@ int compGamesByPrice(const void *a, const void * b) {
 };
 
 int main() {
-    static game * gameArray;
-
     return 0;
 }
