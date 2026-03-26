b9a04ae3566f4484a7a253c2bd83e2b26e54b399
Winter <aurorialis00@gmail.com>
2026-03-26 14:55:49 -0600

    makefile now compiles properly for lab7E1

---
+++ lab7/lab7E1.c
@@ -0,0 +1,29 @@
+/*------------------------------------------
+Student’s Name:             June Bonogofski
+Lab #6                      lab7E1.c
+Lab Section:                X01-L
+Lab Instructor’s Name:      Dhara Wagh
+Lecture Instructor’s Name:  Ardy RD
+--------------------------------------------*/
+
+#include <stdio.h>
+
+typedef struct {
+    double price;
+    char title[25];
+    unsigned int popularity;
+} game;
+
+int compGamesByTitle(const void * a, const void * b) {
+
+};
+
+int compGamesByPrice(const void *a, const void * b) {
+
+};
+
+int main() {
+    static game * gameArray;
+
+    return 0;
+}
