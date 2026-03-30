17aebf0181f321ae61e269038f20b4d91624f426
Bonogofskij <bonogofskij@mymacewan.ca>
2026-03-30 23:15:34 +0000

    Fixed format of gameArray, changed funcs to accept new form. Upload bin to download

--- lab7/lab7E1.c
+++ lab7/lab7E1.c
@@ -17,32 +17,22 @@ typedef struct {
     unsigned int popularity: 3;     //int ranking how popular, scale 1-4
 } game;     //Yay typedefs
 
-//Make a 'game' struct with given values, and return a pointer to the struct
-game * gameNode(double price, char * title, unsigned int popularity) {
-    game * node = malloc(sizeof(game));     //Allocate memory for the node
-    node->price = price;                    //Set node->price to price
-    strcpy(node->title, title);             //copy title to node->title
-    node->popularity = popularity;          //Set node->popularity to popularity
-
-    return node;       //Return pointer to the created node
-}
-
 //Prints the given game array with table formatting
-void printArray(game ** array) {            
+void printArray(game * array) {            
     int i;      //Looping variable
     printf("%-10s%-24s%s\n", "Price", "Title", "Popularity");   //Column headers for user
 
     for (i = 0; i < 6; i++) {       //For each slot in the array
         //Print all its information
-        printf("$%-9.2lf%-24s%u\n", array[i]->price, array[i]->title, array[i]->popularity);
+        printf("$%-9.2lf%-24s%u\n", array[i].price, array[i].title, array[i].popularity);    
     };
     printf("\n");   //Newline for formatting
 }
 
 //qsort argument to sort array by title member
 int compGamesByTitle(const void * a, const void * b) {
-    game *g1 = *(game**)a;      //Cast a as 'game' to a pointer, g1
-    game *g2 = *(game**)b;      //Cast b as 'game' to a pointer, g2
+    game *g1 = (game*)a;      //Cast a as 'game' to a pointer, g1
+    game *g2 = (game*)b;      //Cast b as 'game' to a pointer, g2
 
     //Initialize length to store value of longest string length
     size_t length;
@@ -59,14 +49,15 @@ int compGamesByTitle(const void * a, const void * b) {
 
 //qsort argument to sort array by price member
 int compGamesByPrice(const void * a, const void * b) { 
-    game *g1 = *(game**)a;      //Cast a as 'game' to a pointer, g1
-    game *g2 = *(game**)b;      //Cast b as 'game' to a pointer, g2
-
-    return (int)((g1->price*100) - (g2->price*100));    //Return -1 if a cheaper than b, 0 if equal, etc
+    game *g1 = (game *)a;      //Cast a as 'game' to a pointer, g1
+    game *g2 = (game *)b;      //Cast b as 'game' to a pointer, g2
+    
+    //Return the diff in price for qsort
+    return (int)((g1->price*100) - (g2->price*100));    
 }
 
 //Write the array of games to a binary file called out.bin
-void writeBinary(game ** array) {
+void writeBinary(game * array) {
     FILE *fp;           //Initialize a file pointer
     fp = fopen("out.bin", "wb");    //Open / overwrite 'out.bin' in write-binary mode
 
@@ -100,22 +91,14 @@ void readBinary(game ** array) {
 int main() {
     //Initialize all the nodes to match the given table
     
-    game *node1 = gameNode(22.79, "Opus Magnum", 1);
-    game *node2 = gameNode(0.01, "Minecraft", 4);
-    game *node3 = gameNode(14.99, "Trainz", 2);
-    game *node4 = gameNode(0, "Code Combat", 3);
-    game *node5 = gameNode(7.79, "Lemmings Revolution", 3);
-    game *node6 = gameNode(64.96, "Warcraft", 1);
-
-    static game * gameArray[6];     //Initialize the array
-    
-    //Add all the nodes to the array
-    gameArray[0] = node1;
-    gameArray[1] = node2;
-    gameArray[2] = node3;
-    gameArray[3] = node4;
-    gameArray[4] = node5;
-    gameArray[5] = node6;
+    static game gameArray[6] = {
+        {22.79, "Opus Magnum", 1},
+        {0.01, "Minecraft", 4},
+        {14.99, "Trainz", 2},
+        {0, "Code Combat", 3},
+        {7.79, "Lemmings Revolution", 3},
+        {64.96, "Warcraft", 1}
+    };
 
     //Print the starting array
     printf("\nThe original, unaltered array\n");    //Showing user the context of printArray
@@ -123,7 +106,7 @@ int main() {
 
     //Calculate the number of elements and qsort the array by price
     size_t numGames = sizeof(gameArray) / sizeof(gameArray[0]);
-    qsort(gameArray, numGames, sizeof(gameArray[0]), compGamesByPrice);
+    qsort(gameArray, numGames, sizeof(game), compGamesByPrice);
 
     //Print array sorted by price (ascending)
     printf("The array when sorted by price\n");     //Showing user the context of printArray
@@ -139,7 +122,7 @@ int main() {
     writeBinary(gameArray);
 
     //Make a new array to store the output of out.bin
-    //game * newArray[6];
+    //game newArray[6];
     //readBinary(newArray);
 
     return 0;   //Exit main
